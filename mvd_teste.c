4b43127a957a1d5ddc66b65f0b2cc3b24c282ce5
GolfBravoSierra <giovanigiovani.correio@gmail.com>
2025-10-30 11:20:19 -0300

    vm working in progress

--- mvd_teste.c
+++ mvd_teste.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define buffer 24
 
 enum instructions {ildc, ildv, iadd, isub, imult, idivi,
                    iinv, iand, ior, ineg, icme, icma, iceq,
@@ -16,219 +17,144 @@ typedef struct token{
 int c;
 FILE *file;
 
-void commandPile(int commands[], int *pc){
-    commands[*pc] = c;
-    *pc = *pc + 1;
+void commandPile(token P[], int *i, FILE *file){
+
+    char line [buffer]; 
+    char linebuffer[1000][buffer]; // line vector
+    int linenumb = 0;
+    int numbj = 0;// variable to save the jump number provisionally
+
+    // reading and storing the lines on the line vector
+    while (fgets(line, buffer, file) != NULL){
+        strcpy(linebuffer[linenumb], line);
+        linenumb++;
+    }
+    
+    // debug prints
+    // printf("%d\n", linenumb);
+    // for(int x = 0; x < linenumb; x++){
+    //     printf("%s", linebuffer[x]);
+    // }
+
+    // check for jump commands
+    for(int x = 0; x <= linenumb; x ++){
+        strcpy(line, linebuffer[x]);
+        for (int y = 0; y <= 4; y ++){
+            if (line[y] != NULL){
+                numbj = line[y];
+                line[y] = x;
+                for (int z = 0; z <= linenumb; z ++){
+                    strcpy(line, linebuffer[z]);
+                    for(int w = 0; )
+                }
+            }
+        }
+    }
+
 }
 
-// Função para ler os tk marcar os tipo e arquivalos 
-token commandPile(token commands[], int *pc){
+// pegar coisa do vetor de struct não do arquivo 
+token getInstruction(token P[], int M[], int *s, int *i){
     token tk;
     tk.lexema = (char *) malloc(20 * sizeof(char));
-    int i = 0;
+    //int i = 0;
 
-    while(c != ' ' && c != '\n' && c != EOF){
-        tk.lexema[i] = c;
-        i++;
-        c = fgetc(file);
-    }
-    tk.lexema[i] = '\0';
-    if(c == ' '){
-        c = fgetc(file);
-    }
-    if(c == '\n'){
-        c = fgetc(file);
-    }
-    if (strcmp(tk.lexema, "ildc") == 0){
-        tk.tipo = ildc;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "ildv") == 0){
-        tk.tipo = ildv;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "iadd") == 0){
-        tk.tipo = iadd;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "isub") == 0){
-        tk.tipo = isub;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "imult") == 0){
-        tk.tipo = imult;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "idivi") == 0){
-        tk.tipo = idivi;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "iinv") == 0){
-        tk.tipo = iinv;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "iand") == 0){
-        tk.tipo = iand;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "ior") == 0){
-        tk.tipo = ior;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "ineg") == 0){
-        tk.tipo = ineg;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "icme") == 0){
-        tk.tipo = icme;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "icma") == 0){
-        tk.tipo = icma;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "iceq") == 0){
-        tk.tipo = iceq;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "icdif") == 0){
-        tk.tipo = icdif;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "icmeq") == 0){
-        tk.tipo = icmeq;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "icmaq") == 0){
-        tk.tipo = icmaq;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "ijmp") == 0){
-        tk.tipo = ijmp;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "ijmpf") == 0){
-        tk.tipo = ijmpf;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    } else if (strcmp(tk.lexema, "inum") == 0){
-        tk.tipo = inum;
-        commands[*pc] = tk;
-        *pc = *pc + 1;
-    }
-
-    c = fgetc(file);
-
-    return tk;
-}
-
-token getInstruction(int execpile[], int *s){
-    token tk;
-    tk.lexema = (char *) malloc(20 * sizeof(char));
-    int i = 0;
-
-    while(c != ' ' && c != '\n' && c != EOF){
-        tk.lexema[i] = c;
-        i++;
-        c = fgetc(file);
-    }
-    tk.lexema[i] = '\0';
-    if(c == ' '){
-        c = fgetc(file);
-    }
-    if(c == '\n'){
-        c = fgetc(file);
-    }
     if(strcmp(tk.lexema, "ildc") == 0){
         *s = *s + 1; 
-        execpile[*s] = c;
+        M[*s] = c;
     } else if(strcmp(tk.lexema, "ildv") == 0){
         *s = *s + 1;
-        execpile[*s] = c;
+        M[*s] = c;
     } else if(strcmp(tk.lexema, "iadd") == 0){
-        execpile[*s - 1] = execpile[*s - 1] + execpile[*s];
+        M[*s - 1] = M[*s - 1] + M[*s];
         *s = *s - 1;
     } else if(strcmp(tk.lexema, "isub") == 0){
-        execpile[*s - 1] = execpile[*s - 1] - execpile[*s];
+        M[*s - 1] = M[*s - 1] - M[*s];
         *s = *s - 1;
     } else if(strcmp(tk.lexema, "imult") == 0){
-        execpile[*s - 1] = execpile[*s - 1] * execpile[*s];
+        M[*s - 1] = M[*s - 1] * M[*s];
         *s = *s - 1;
     } else if(strcmp(tk.lexema, "idivi") == 0){
-        execpile[*s - 1] = execpile[*s - 1] / execpile[*s];
+        M[*s - 1] = M[*s - 1] / M[*s];
         *s = *s - 1;
     } else if(strcmp(tk.lexema, "iinv") == 0){
-        execpile[*s] = -(execpile[*s]);
+        M[*s] = -(M[*s]);
     } else if(strcmp(tk.lexema, "iand") == 0){
-        if(execpile[*s - 1] == 1 && execpile[*s] == 1){
-			execpile[*s - 1] = 1;
+        if(M[*s - 1] == 1 && M[*s] == 1){
+			M[*s - 1] = 1;
 		}
 		else{
-			execpile[*s - 1] = 0;
+			M[*s - 1] = 0;
 			*s = *s - 1;
 		}
     } else if(strcmp(tk.lexema, "ior") == 0){
-        if(execpile[*s - 1] == 1 || execpile[*s] == 1){
-			execpile[*s - 1] = 1;
+        if(M[*s - 1] == 1 || M[*s] == 1){
+			M[*s - 1] = 1;
 		}
 		else{
-			execpile[*s - 1] = 0;
+			M[*s - 1] = 0;
 			*s = *s - 1;
 		}
     } else if(strcmp(tk.lexema, "ineg") == 0){
-		execpile[*s] = 1 - execpile[*s];
+		M[*s] = 1 - M[*s];
     } else if(strcmp(tk.lexema, "icme") == 0){
-        if(execpile[*s - 1] < execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] < M[*s]){
+            M[*s - 1] = 1;
         }
         else {
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }
     } else if(strcmp(tk.lexema, "icma") == 0){
-        if(execpile[*s - 1] > execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] > M[*s]){
+            M[*s - 1] = 1;
         }
         else{
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }
     } else if(strcmp(tk.lexema, "iceq") == 0){
-        if(execpile[*s - 1] == execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] == M[*s]){
+            M[*s - 1] = 1;
         }
         else{
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }
     } else if(strcmp(tk.lexema, "icdif") == 0){
-        if(execpile[*s - 1] != execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] != M[*s]){
+            M[*s - 1] = 1;
         }
         else{
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }        
     } else if(strcmp(tk.lexema, "icmeq") == 0){
-        if(execpile[*s - 1] <= execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] <= M[*s]){
+            M[*s - 1] = 1;
         }
         else{
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }
     } else if(strcmp(tk.lexema, "icmaq") == 0){
-        if(execpile[*s - 1] >= execpile[*s]){
-            execpile[*s - 1] = 1;
+        if(M[*s - 1] >= M[*s]){
+            M[*s - 1] = 1;
         }
         else{
-            execpile[*s - 1] = 0;
+            M[*s - 1] = 0;
             *s = *s - 1;
         }
-    } //else if (strcmp(tk.lexema, "ijmp") == 0){
-       
+    } 
+    // else if (strcmp(tk.lexema, "ijmp") == 0){
+    //     *s = c; 
     // } else if (strcmp(tk.lexema, "ijmpf") == 0){
-
+    //     if ((M[*s] = 0) ){
+    //         *s = c;
+    //     }
+    //     else{
+    //         *s =  + 1;
+    //     }
     // } else if (strcmp(tk.lexema, "inum") == 0){
         
     // }
@@ -236,7 +162,6 @@ token getInstruction(int execpile[], int *s){
 
     c = fgetc(file);
     
-    return tk;
 }
 
 int main(int argc, char *argv[])
@@ -246,22 +171,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int execpile[1000], s = -1, pc = 0;
-    token commands[1000];
+    int M[1000];
+    int i = 0; // registrador do programa
+    int s = 0; // registrador da pilha de valores
+    char c;
+    token P[1000];
 
     file = fopen(argv[1], "r");
 
-    c = fgetc(file); 
+    commandPile(P, &i, file);
 
-    // Primeiro leio os tk marco o tipo e armazeno em uma pilha para 
-    // depois durante a interpretação se tiver um jmp ele busca o tk para onde 
-    // o programa saltou nessa pilha
-    while (c != EOF){
-        commandPile(commands, &pc);
-    }
-
-    while(c != EOF){
-        getInstruction(execpile, &s);
+    while(i != i){
+        getInstruction(P, M, &s, &i);
+        i = i + 1;
     }
     return 0;
 }
